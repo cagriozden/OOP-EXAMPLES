ba2155fd5a3026ff864fd59efe880bc1a8c9b5a2
Çağrı Özden <cagri.ozden98@gmail.com>
2024-01-25 15:57:48 +0300

    StaticConstructor-Constructor
    StaticConstructor-Constructor

---
+++ StaticConstructor_Constructor/StaticConstructor_Constructor/Program.cs
@@ -0,0 +1,45 @@
+﻿// Seeusing System;
+
+using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        cagri c = new cagri();  
+        // öncelikle static constructor çalışır arkasıdnan constructor çalışır
+
+        cagri c2= new cagri();  
+        // sadece constructor çalışır - static constructor ilk defa nesne oluşturulurken çalışır)
+        
+
+    }
+
+    class cagri
+    {
+        public cagri()
+        {
+            //Constructor 
+            // Bir sınıfta nesne oluşrulurken ilk tetiklenen fonksiyondur.
+            // ilgili sınıftan her nesne üretilirken tetiklenme durumu olur.
+
+            Console.WriteLine("Constructor Çalıştı");
+        
+        }
+
+        static cagri()
+        {
+            //Static Constructor bir sınıftan ilk nesne oluştuğunda çalışan fonksiyondur
+            //Static Constructor geri dönüş değeri ve erişim belirleyecisi bildirmez.
+            //Parametre almaz
+
+            Console.WriteLine("Static Constructor Çalıştı");
+        }
+
+    }
+
+}
+
+
