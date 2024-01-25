c7568e4cf20a902bbdd68c6eb4bbe5a1aa7958dd
Çağrı Özden <cagri.ozden98@gmail.com>
2024-01-25 16:18:20 +0300

    Encapsulation_Examples
    Encapsulation_Examples

---
+++ Encapsulation_Examples/Encapsulation_Examples/Program.cs
@@ -0,0 +1,36 @@
+﻿// Seeusing System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        cagri c=new cagri();
+
+        c.MyProperty = 12; // set kısmında atamanın yapılması
+
+        Console.WriteLine(c.MyProperty); // get kısmında atanan değerin okunması
+    }
+
+    class cagri
+    {
+        private int sayi1;
+        // Kapsüllemenin yapıldığı field private olarak belirtilmiştir.
+
+        public int MyProperty
+        {
+            get 
+            {
+                return sayi1;
+            }
+            set 
+            {
+                sayi1 = value;  
+            }
+        }
+
+    }
+}
+
+
+
+
